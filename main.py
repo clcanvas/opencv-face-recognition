4035a2dbe08e91b38b41558e352ef2514ce30da8
nishc <nishchai@outlook.com>
2024-02-18 18:11:48 -0500

    initial files

---
+++ main.py
@@ -0,0 +1,6 @@
+import face_recognition # face recog library
+import os # operating system control
+import numpy as num # 2d array data control
+import cv2 # opencv library for managing data
+
+input_video_path = "test.mp4"
